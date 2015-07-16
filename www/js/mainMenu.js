8810b9d8cf9355127ccee3ab8dea4deba2f6b6a0
Mohammad Hajj SHehadeh <m.hajjshehadeh@gmail.com>
2015-07-16 12:14:14 +0200

    Adds main menu screen
    Adds buttons for the following screens:
    Play Now
    Settings
    About Us
    Leaderboard
    Exit
    
    Conflicts:
    	www/index.html

--- www/js/mainMenu.js
+++ www/js/mainMenu.js
@@ -4,3 +4,22 @@
 
 // write code DOWN from here.
 
+function playmenu(){
+    alert("Let's play");
+}
+
+function settingsmenu(){
+    alert("settings menu,you");
+}
+
+function aboutusmenu(){
+    alert("App was created by");
+}
+
+function leaderboardmenu(){
+    alert("You are nothing but a leader");
+}
+
+function exitapp(){
+    alert("Have Fun");
+}
