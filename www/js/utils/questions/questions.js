99948a54551d1f0e10d0b89b67d36330bd8978df
Ekaterina Dubinkina <katya_dubinkina@mail.ru>
2015-07-16 14:53:25 +0200

    Adds a list of questions for the game

---
+++ www/js/utils/questions/questions.js
@@ -0,0 +1,78 @@
+var list_questions = [
+    {question: "blalbla", correct_answer: "", all_answer: ["", "", "", ""]},
+   
+    {question: "What's the worlds most popular Non Alcoholic Drink?", correct_answer: "Coffee", all_answer: ["Tea","Coca-Cola","Coffee","Sprite"]},
+    
+    {question: "How many stars are on the European Union flag?", correct_answer:"12", all_answer:["15","12","20","21"]},
+    
+    {question: "Which country in the world sends the most letters abroad?", correct_answer: "UK", all_answer: ["Germany","USA","Russia","UK"]},
+   
+    {question: "Which number president of the USA is George W Bush?", correct_answer: "43", all_answer: ["40","39","32","43"]},
+    
+    {question: "Lending her name to a famous brand, who was the Greek Goddess of Victory?", correct_answer: "Nike", all_answer: ["Adidas","Nike","Reebok","Puma"]},
+
+    {question: "How many bits are there in a byte?", correct_answer: "8", all_answer:["8","10","11","5"]},
+
+    {question: "Which planet has an orbital period of 687 days?", correct_answer: "Mars", all_question: ["Venus","Jupiter","Mars"]},
+
+    {question: "In which organ of the body is the pineal gland?", correct_answer: "Brain", all_answer: ["Lungs","Liver","Heart","Brain"]},
+  
+    {question: "Who was the first pilot to exceed the speed of sound? Charles Yeager?", correct_answer: "Charles Yeager", all_answer: ["James Watt", "David Livingstone", "Donald Campbell", "Charles Yeage"]},
+   
+    {question: "Who was the first pilot to exceed the speed of sound? Charles Yeager?", correct_answer: "Charles Yeager", all_answer: ["James Watt", "David Livingstone", "Donald Campbell", "Charles Yeage"]},
+    
+    {question: "In what sport do players take long and short corners", correct_answer: "Hockey", all_answer: ["Football", "Valleyball", "Socket", "Hockey"]},
+        
+{question: "What is the 'perfect score' in a game of Ten Pin Bowling?", correct_answer: "300", all_answer: ["333", "300", "211", "199"]},
+    
+{question: "In which country is basketball a major sport?", correct_answer: "USA", all_answer: ["UK", "Canada", "USA", "Russia"]},
+
+    {question: "What is the term for a zero score in tennis?", correct_answer: "Love", all_answer: ["Love", "Naught", "Zero itself", "Let"]},
+
+    {question: "What is the name of the governing body in the world top basket ball league?", correct_answer: "NBA", all_answer: ["MLB", "NBA", "NHL", "MLD"]},
+
+    {question: "Which member of the British Royal Family presents the Prizes to the winners at Wimbledon?", correct_answer: "Duke of Kent", all_answer: ["Duke of Somerset", "The Reigning Monarch", "Prince of Wales", "Duke of Kent"]},
+    
+    {question: "What is Ozzy Osborne' real first name?", correct_answer: "John", all_answer: ["William", "John", "Jackson", "Paul"]},
+    
+    {question: "In which year did Robbie Williams leave Take That?", correct_answer: "1995", all_answer: ["1995", "1999", "2001", "1992"]},
+    
+    {question: "Charles Bronson who died in 2003 was best know for the film Death Wish. In which year was it released? ", correct_answer: "1974", all_answer: ["1970", "1974", "1975", "1800"]},
+    
+    {question: "The Matrix reloaded took $93 mil in its opening US weekend, a record secondly only to which previous film? ", correct_answer: "Spiderman", all_answer: ["Harry Potter", "Lord of the Rings", "Shrek", "Spiderman"]},
+    
+    {question: "What film remake is Lord of the Rings director Peter Jackson currently working on? ", correct_answer: "King Kong", all_answer: ["Harry Potter", "Batman", "Star Wars", "King Kong"]},
+    
+    {question: "Who was the youngest president of the USA?", correct_answer: "Theodore Roosevel", all_answer: ["Grover Cleveland", "Warren G. Harding", "Theodore Roosevel", "Herbert Hoover"]},
+    
+    {question: "Who designed the dome of St. Peter's, Rome?", correct_answer: "Michelangelo", all_answer: ["Giacomo della Porta", "Michelangelo", "Leonardo Da Vinchi"]},
+    
+    {question: "In what year was Mahatma Gandhi assassinated? ", correct_answer: "1948", all_answer: ["1950", "1939", "1960", "1948"]},
+    
+    {question: "In which year did Hitler become chancellor of Germany? ", correct_answer: "1933", all_answer: ["1940", "1933", "1920", "1938"]},
+    
+    {question: "In which year was Nato formed? ", correct_answer: "1949", all_answer: ["1949", "1945", "1950", "1933"]},
+    
+    {question: "What, in population terms, is the largest Spanish speaking country in the world?", correct_answer: "Mexico", all_answer: ["Mexico", "Spain", "Chile", "Portugal"]},
+    
+    {question: "In which country would you find both the rivers Oder and Vistula, which flow into the Baltic Sea?", correct_answer: "Poland", all_answer: ["Poland", "Serbia", "Czech republic", "Germany"]},
+    
+    
+    {question: "What is the capital of Iran?", correct_answer: "Tehran", all_answer: ["Kerman", "Yazd", "Shiraz", "Tehran"]},
+    
+    
+    {question: "Paraguay has borders with Argentina, Brazil and which other country?", correct_answer: "Bolivia", all_answer: ["Chile", "Bolivia", "Suriname", "Peru"]},
+    
+    
+    {question: "Rabat is the capital of which Kingdon?", correct_answer: "Morocco", all_answer: ["Morocco", "UK", "Spain", "Norway"]},
+    
+        
+    {question: "World record speed attained in a helicopter?", correct_answer: "249 mph", all_answer: ["353 mph", "180 mph", "249 mph", "260 mph"]},
+    
+        
+    {question: "In which year was dynamite invented?", correct_answer: "1866", all_answer: ["1789", "1890", "1866", "1904"]},
+    
+        
+    {question: "What is the 'perfect score' in a game of Ten Pin Bowling?", correct_answer: "300", all_answer: ["333", "300", "211", "199"]},
+     
+    ];
