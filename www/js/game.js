a1d2a3d6432a91d109dc033eaeb25f255a2a8552
Paula Rodriguez Cardenas <paulauc3m@gmail.com>
2015-07-16 15:26:34 +0200

    Develops the game state
    Adds some CSS style for the possible answers, right answer,
    wrong answer.
    Adds handles for answer choosing and setting the appropriate
    CSS style depending if the answer is correct or not.

--- www/js/game.js
+++ www/js/game.js
@@ -4,3 +4,14 @@
 
 // write code DOWN from here.
 
+function correct(answer) {
+    answer.style.backgroundColor= "#82C15B";
+    alert("The answer is true, Congratulations!!");
+    answer.style.backgroundColor="#26ABE2";
+}
+
+function incorrect(answer){
+    answer.style.backgroundColor="#FF0080";
+    alert("The answer is false! Sorry :(");
+    answer.style.backgroundColor="#11A8D6";
+}
